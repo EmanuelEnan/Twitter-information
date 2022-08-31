ff8d55804fc5af10a0d593f1ae4cb823b7150ac0
EmanuelEnan <emaanuelenan@gmail.com>
2022-08-31 15:16:33 +0600

    updated

--- lib/others/tokens.dart
+++
@@ -1,8 +0,0 @@
-class Tokens {
-  String bearerToken =
-      "Bearer AAAAAAAAAAAAAAAAAAAAAD7PeAEAAAAAPKX1Rnj9LDQYDvh1ouI605qT0Lc%3DZdcBEPpDRopHycGjbve5JuYrXCx0WoS7OPh7tFyV2xM8SACVxX";
-
-  String url = "https://api.twitter.com/2/users/by/username/";
-
-  String urlLocation = "https://api.twitter.com/2/users/3302275273/followers";
-}
